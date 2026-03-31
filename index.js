e03f2016515dbb1d184610f57260f86a678b2199
tingamapuro04 <alponcemobutu100@gmail.com>
2026-03-31 07:45:00 +0300

    Connect mongo database

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import express from express;
-require('dotenv').config()
-import errorHandler from './middlewares/errorHandler';
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import errorHandler from './middlewares/errorHandler.js';
+import dbConnect from './utils/db.js'
 
+dbConnect();
+const port = process.env.PORT || 4000
 const app = express();
-
 app.use(errorHandler)
 app.listen(port, () => {
   console.log("Server running.")
--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+
+const dbConnect = async () => {
+  try{
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("Connection successfull")
+  }catch(err){
+    console.error(err.message)
+    process.exit(1)
+  }
+}
+
+export default dbConnect;
